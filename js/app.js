e9368d1a5e766e0f7d42a9a0abac00d1304803e9
Devjhoti <swaccha01@gmail.com>
2022-03-27 01:34:13 +0600

    Final upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // console.log('hello');
 
-const btn=document.getElementById('download');
+const btn = document.getElementById('download');
 
-btn.addEventListener('click',function(){
-    window.location.assign('./pdf/Curriculum Vitae of Anindya Baul Mridul final.pdf');
+btn.addEventListener('click', function () {
+    window.location.assign("https://drive.google.com/uc?export=download&id=1h3BfmDuCp9sPZ1SBdZ9HlTqrhmBxF7fZ");
 })
